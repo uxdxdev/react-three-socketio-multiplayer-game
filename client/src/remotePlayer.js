09cced9329ae88803e41d6170c6cef1adfcc3813
david.morton <david.morton@servicenow.com>
2022-01-21 16:38:46 +0000

    fixes for remote players

--- client/src/remotePlayer.js
+++ client/src/remotePlayer.js
@@ -1,15 +1,6 @@
-import { useRef, memo } from 'react';
-import { useFrame } from '@react-three/fiber';
+import { memo, useRef } from 'react';
 import { Character } from './character';
 
-export const RemotePlayer = memo(({ position, rotation, moving }) => {
-  const ref = useRef();
-  // set the remote players initial position
-  const initPosition = useRef(position);
-  useFrame(() => {
-    ref.current.position.x = position[0];
-    ref.current.position.z = position[2];
-  });
-
-  return <Character ref={ref} moving={moving} position={initPosition.current} rotation={rotation} />;
+export const RemotePlayer = memo((props) => {
+  return <Character ref={useRef()} {...props} />;
 });
