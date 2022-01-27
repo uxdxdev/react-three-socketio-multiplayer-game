70633a8196a57ee4633dd5ecadc643c8cbe74c54
David Morton <david.morton@servicenow.com>
2022-01-27 20:20:52 +0000

    added shadows to player

--- client/src/character.js
+++ client/src/character.js
@@ -24,17 +24,17 @@ export const Character = memo(
     return (
       <group ref={ref} position={position} rotation={[0, rotation, 0]} dispose={null} scale={[1, 1, 1]}>
         <primitive object={nodes.Root} />
-        <skinnedMesh geometry={nodes.CUBezierCurve000.geometry} material={nodes.CUBezierCurve000.material} skeleton={nodes.CUBezierCurve000.skeleton} />
-        <skinnedMesh geometry={nodes.CUBezierCurve000_1.geometry} material={nodes.CUBezierCurve000_1.material} skeleton={nodes.CUBezierCurve000_1.skeleton} />
-        <skinnedMesh geometry={nodes.CUBezierCurve002.geometry} material={nodes.CUBezierCurve002.material} skeleton={nodes.CUBezierCurve002.skeleton} />
-        <skinnedMesh geometry={nodes.CUBezierCurve002_1.geometry} material={nodes.CUBezierCurve002_1.material} skeleton={nodes.CUBezierCurve002_1.skeleton} />
-        <skinnedMesh geometry={nodes.CUBezierCurve002_2.geometry} material={materials.Main2} skeleton={nodes.CUBezierCurve002_2.skeleton} />
-        <skinnedMesh geometry={nodes.Ears.geometry} material={nodes.Ears.material} skeleton={nodes.Ears.skeleton} />
-        <skinnedMesh geometry={nodes.CUBezierCurve003.geometry} material={nodes.CUBezierCurve003.material} skeleton={nodes.CUBezierCurve003.skeleton} />
-        <skinnedMesh geometry={nodes.CUBezierCurve003_1.geometry} material={materials.Black} skeleton={nodes.CUBezierCurve003_1.skeleton} />
-        <skinnedMesh geometry={nodes.CUBezierCurve003_2.geometry} material={nodes.CUBezierCurve003_2.material} skeleton={nodes.CUBezierCurve003_2.skeleton} />
-        <skinnedMesh geometry={nodes.CUBezierCurve003_3.geometry} material={materials.White} skeleton={nodes.CUBezierCurve003_3.skeleton} />
-        <skinnedMesh geometry={nodes.CUBezierCurve003_4.geometry} material={materials.EyeColor} skeleton={nodes.CUBezierCurve003_4.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.CUBezierCurve000.geometry} material={nodes.CUBezierCurve000.material} skeleton={nodes.CUBezierCurve000.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.CUBezierCurve000_1.geometry} material={nodes.CUBezierCurve000_1.material} skeleton={nodes.CUBezierCurve000_1.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.CUBezierCurve002.geometry} material={nodes.CUBezierCurve002.material} skeleton={nodes.CUBezierCurve002.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.CUBezierCurve002_1.geometry} material={nodes.CUBezierCurve002_1.material} skeleton={nodes.CUBezierCurve002_1.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.CUBezierCurve002_2.geometry} material={materials.Main2} skeleton={nodes.CUBezierCurve002_2.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.Ears.geometry} material={nodes.Ears.material} skeleton={nodes.Ears.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.CUBezierCurve003.geometry} material={nodes.CUBezierCurve003.material} skeleton={nodes.CUBezierCurve003.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.CUBezierCurve003_1.geometry} material={materials.Black} skeleton={nodes.CUBezierCurve003_1.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.CUBezierCurve003_2.geometry} material={nodes.CUBezierCurve003_2.material} skeleton={nodes.CUBezierCurve003_2.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.CUBezierCurve003_3.geometry} material={materials.White} skeleton={nodes.CUBezierCurve003_3.skeleton} />
+        <skinnedMesh castShadow receiveShadow geometry={nodes.CUBezierCurve003_4.geometry} material={materials.EyeColor} skeleton={nodes.CUBezierCurve003_4.skeleton} />
       </group>
     );
   })
